524dbd4a5f13d476e18dfebdcaed4ddcfb560f4d
imYarr <146012359+imYarr@users.noreply.github.com>
2024-10-02 14:24:24 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#1st program
+print(12+89-1)
+#2st program
+print(9.99>9.98 and 1000 !=1000.1)
+#3st program
+print(2*2+2)
+print(2*(2+2))
+print(4==8)
+#4st program
+float('123.456')
+print(int((123.456*10) % 10))
+
+
